82b278b02144c7209f41bfd38f91e0820e9a27e0
JasonMarkWomack <JasonMarkWomack@users.noreply.github.com>
2022-11-07 14:03:55 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function App() {
 	useEffect(() => {
 		setInterval(() => {
 			const now = moment();
-			const then = moment('2020-10-15 12:12:12', 'YYYY-MM-DD hh:mm:ss');
+			const then = moment('2023-12-01 12:12:12', 'YYYY-MM-DD hh:mm:ss');
 			const countdown = moment(then - now);
 			setDays(countdown.format('D'));
 			setHours(countdown.format('HH'));
@@ -25,8 +25,9 @@ function App() {
 
 	return (
 		<div className="app">
-			<h1 className="title">Countdown Timer</h1>
-			<div className="timer-container">
+			<h1 className="title">Club Real Estate</h1>
+			<p>By your next home on-chain!</p>
+			<a class="button" href="https://zu0h740xp5l.typeform.com/to/EBs0WgwX">Join the waitlist...</a>			<div className="timer-container">
 				<div className="timer">
 					{days}
 					<span>days</span>
